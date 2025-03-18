6a5533b60cf30ab25f60e9cdfcff30036e052d0b
Naveen pandian <npandian515@gmail.com>
2025-03-18 07:56:40 +0530

    feat: add useEthersSigner hook for integrating ethers.js with wagmi client

---
+++ sociofi/src/hooks/useEthersSigner.js
@@ -0,0 +1,30 @@
+import {BrowserProvider, JsonRpcSigner} from "ethers";
+import { useMemo } from "react";
+import { useConnectorClient } from "wagmi";
+
+export const clientToSigner = (client) => {
+    const {account , chain , transport } = client
+
+    const network = {
+        chainId : chain.id,
+        name : chain.name,
+        ensAddress : chain.contracts?.ensRegistry?.address,
+    };
+  
+     const  provider = new BrowserProvider(transport, network);
+
+     const signer = new JsonRpcSigner(provider , account.address);
+
+     return signer;
+
+};
+
+
+ export const useEthersSigner = ({chainId}) =>{
+    const {data : client} = useConnectorClient({chainId});
+
+    return useMemo( () => (
+        client ? clientToSigner(client) : undefined
+    ),[client]);
+
+ }
